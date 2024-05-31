a511728a9782401860d19aa07007d7a5422583b6
ItzMiracleOwO <miraclecutelemon@gmail.com>
2024-05-31 01:50:01 +0000

    Example config

---
+++ config.example.js
@@ -0,0 +1,8 @@
+module.exports = {
+    clientId: process.env.clientId || "Your client ID", // Get from Discord develoepr portal 
+    token:
+      process.env.token ||
+      "Your token", // Get from Discord develoepr portal
+    port: process.env.port || 3000, // Your banner server port
+  };
+  
